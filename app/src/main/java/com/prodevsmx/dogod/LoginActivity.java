c41ee6c30cbaa79ac069c9b480630a6050c2e86e
DiegoRamirezGuerrero <diegormz111@gmail.com>
2017-04-08 19:42:46 -0500

    LoginActivity

--- app/src/main/java/com/prodevsmx/dogod/LoginActivity.java
+++ app/src/main/java/com/prodevsmx/dogod/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.prodevsmx.dogod;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
+
+    Typeface aliens;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+
+        title = (TextView) findViewById(R.id.tvTitle);
+
+        //fonts
+        aliens = Typeface.createFromAsset(getAssets(), "fonts/aliens.ttf");
+
+        title.setTypeface(aliens);
+
     }
 }
+
