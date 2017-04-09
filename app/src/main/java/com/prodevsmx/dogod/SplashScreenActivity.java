9779aeb986b294e3c75b790d49e36c214bb317cd
RafaelPasos <alirafael.pasos@gmail.com>
2017-04-08 19:15:54 -0500

    Adding application colors

---
+++ app/src/main/java/com/prodevsmx/dogod/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.prodevsmx.dogod;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+        Thread splashscreenspleep = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        splashscreenspleep.start();
+    }
+}
