19ec1f7d37e94ce4f6a76ad21ac70041a0a0d8cf
RafaelPasos <alirafael.pasos@gmail.com>
2017-04-09 19:58:49 -0500

    Adding Register Activity

--- app/src/main/java/com/prodevsmx/dogod/LoginActivity.java
+++ app/src/main/java/com/prodevsmx/dogod/LoginActivity.java
@@ -1,13 +1,20 @@
 package com.prodevsmx.dogod;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Typeface aliens;
+    TextView JoinUs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
 }
