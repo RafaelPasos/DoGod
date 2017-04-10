a934aa1c15c1929e6eae49c692a8432807f4bb25
RafaelPasos <alirafael.pasos@gmail.com>
2017-04-09 20:05:56 -0500

    Adding Register Activity

--- app/src/main/java/com/prodevsmx/dogod/NewAccountActivity.java
+++ app/src/main/java/com/prodevsmx/dogod/NewAccountActivity.java
@@ -1,13 +1,28 @@
 package com.prodevsmx.dogod;
 
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NewAccountActivity extends AppCompatActivity {
+
+    Typeface aliens;
+    TextView JoinUs;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
+
+        aliens = Typeface.createFromAsset(getAssets(), "fonts/aliens.ttf");
+        JoinUs.setTypeface(aliens);
+        JoinUs =  (TextView) findViewById(R.id.tvJoinUs);
+
     }
+
+
 }
