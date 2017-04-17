43bdd8742046440d740b1d93e09255249e828262
Diego Gustavo Ramirez Guerrero <diegormz111@gmail.com>
2017-04-17 14:47:05 -0500

    Fix 5.0 Create new account implemented

--- app/src/main/java/com/prodevsmx/dogod/LoginActivity.java
+++ app/src/main/java/com/prodevsmx/dogod/LoginActivity.java
@@ -1,15 +1,22 @@
 package com.prodevsmx.dogod;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class LoginActivity extends Activity {
+
     TextView title;
     Typeface aliens;
+
+    TextView createNewAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +25,21 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-
+        //id declarations
         title = (TextView) findViewById(R.id.tvTitle);
+        createNewAccount = (TextView) findViewById(R.id.tvCreateAccount);
 
         //fonts
         aliens = Typeface.createFromAsset(getAssets(), "fonts/aliens.ttf");
-
         title.setTypeface(aliens);
+
+        //Go to create new account
+        createNewAccount.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent createAccount = new Intent(LoginActivity.this, NewAccountActivity.class);
+                startActivity(createAccount);
+            }
+        });
 
     }
 
--- app/src/main/java/com/prodevsmx/dogod/NewAccountActivity.java
+++ app/src/main/java/com/prodevsmx/dogod/NewAccountActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.TextView;
 
 public class NewAccountActivity extends Activity {
@@ -14,12 +15,17 @@ public class NewAccountActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_new_account);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        //id fonts declarations
+        JoinUs = (TextView) findViewById(R.id.tvJoinUs);
+
+        //fonts
         aliens = Typeface.createFromAsset(getAssets(), "fonts/aliens.ttf");
         JoinUs.setTypeface(aliens);
-        JoinUs =  (TextView) findViewById(R.id.tvJoinUs);
+
 
     }
 
