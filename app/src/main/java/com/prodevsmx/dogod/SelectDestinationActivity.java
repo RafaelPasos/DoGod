2eb2d1b3a57a30379f27cb5f48c5dc4af52dfb84
RafaelPasos <alirafael.pasos@gmail.com>
2017-04-18 13:33:09 -0500

    Displaying mapFragment of Burj Khalifa

--- app/src/main/java/com/prodevsmx/dogod/SelectDestinationActivity.java
+++ app/src/main/java/com/prodevsmx/dogod/SelectDestinationActivity.java
@@ -4,12 +4,14 @@ import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 public class SelectDestinationActivity extends AppCompatActivity implements OnMapReadyCallback {
-
 
     GoogleMap mapGoogleMap;
 
@@ -31,5 +33,14 @@ public class SelectDestinationActivity extends AppCompatActivity implements OnMa
     @Override
     public void onMapReady(GoogleMap mGoogleMap) {
         mapGoogleMap = mGoogleMap;
+        goToLocation(25.1941287,55.2736491,16);
     }
+
+    private void goToLocation(double lat, double lng, float zoom) {
+        LatLng location = new LatLng(lat,lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(location,zoom);
+        mapGoogleMap.moveCamera(update);
+    }
+
+
 }
