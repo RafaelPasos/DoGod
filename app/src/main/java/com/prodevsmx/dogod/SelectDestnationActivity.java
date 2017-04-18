a1e5479149ff863711de56815f2900852b037c43
RafaelPasos <alirafael.pasos@gmail.com>
2017-04-17 20:05:05 -0500

    Map fragment is now displayed

--- app/src/main/java/com/prodevsmx/dogod/SelectDestnationActivity.java
+++ app/src/main/java/com/prodevsmx/dogod/SelectDestnationActivity.java
@@ -4,7 +4,14 @@ import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SelectDestnationActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class SelectDestnationActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    GoogleMap mapGoogleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,17 @@ public class SelectDestnationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_select_destnation);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        startMap();
+    }
+
+    private void startMap(){
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap mGoogleMap) {
+        mapGoogleMap = mGoogleMap;
     }
 }
